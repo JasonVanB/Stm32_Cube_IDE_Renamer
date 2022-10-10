52a4d8e7e83464d59c2ea0094e142e761ea5e4bb
Jason Van Beusekom <vanbe095@umn.edu>
2022-10-10 17:58:27 -0500

    started work on implementation

--- stm_renamer.py
+++ stm_renamer.py
@@ -4,40 +4,49 @@ import os
 dpg.create_context()
 
 # this is ugly code but since this is a simple gui / program I do not care about preformance / modularity
-def callback(sender, app_data):
-    new_name = "test"
+def get_folder(sender, app_data):
+    new_name = dpg.get_value("new_name_input")
     folder_path = app_data["file_path_name"]
-    split = folder_path.split("\\")
-    old_name = split[-1]
-    print("old name: ", old_name)
-    print(split)
-    print("Folder path: ", folder_path)
+    old_name = folder_path.split("\\")[-1]
+
+    # if there is one or more ioc files, error out may want to add additional functionality for this
+    # to allow renaming mutiple ioc files at once if needed
     file_list = []
     for file in os.listdir(folder_path):
         if file.endswith(".ioc"):
             file_list.append(file)
-            print(file)
-            with open(folder_path + "\\" + file, "r") as f:
-                filedata = f.read()
-            filedata = filedata.replace(old_name, new_name)
-            with open(folder_path + "\\" + file, "w") as f:
-                f.write(filedata)
-            os.rename( folder_path + "\\" + file, folder_path + "\\" + file.replace(old_name, new_name))
-    new_folder_path = folder_path.replace(old_name, new_name)
-    if len(file_list) > 0:
-        print(folder_path)
-        print(new_folder_path)
+
+    if len(file_list) == 1:
+
+        with open(folder_path + "\\" + file_list[0], "r") as f:
+            filedata = f.read()
+
+        filedata = filedata.replace(old_name, new_name)
+
+        with open(folder_path + "\\" + file_list[0], "w") as f:
+            f.write(filedata)
+
+        os.rename( folder_path + "\\" + file_list[0], folder_path + "\\" + file_list[0].replace(old_name, new_name))
+
+        new_folder_path = folder_path.replace(old_name, new_name)
         os.rename(folder_path, new_folder_path)
+    elif len(file_list) == 0:
+        print("no ioc files found")
+    else:
+        print("multiple ioc files found")
+        
     
 
     
 
 
-dpg.add_file_dialog(directory_selector=True, show=False, callback=callback, tag="file_dialog_id", height=300, width=600)
+dpg.add_file_dialog(directory_selector=True, show=False, callback=get_folder, tag="file_dialog_id", height=300, width=600)
 
 with dpg.window(label="Tutorial",tag = "main_window" , width=800, height=300):
-    dpg.add_text("To use press the button, select the folder of the project you want to rename, input the new desiered name")
-    dpg.add_button(label="Directory Selector", callback=lambda: dpg.show_item("file_dialog_id"))
+    dpg.add_text("To use, input the name you want to change the project to and press the button to select the folder")
+    with dpg.group(horizontal=True):
+        new_name_input = dpg.add_input_text(tag = "new_name_input")
+        dpg.add_button(label="Directory Selector", callback=lambda: dpg.show_item("file_dialog_id"))
     
 dpg.set_primary_window("main_window", True)
 dpg.create_viewport(title='Custom Title', width=800, height=600)
